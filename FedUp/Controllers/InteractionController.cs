347184bbabee98a3bc5d8e1f70490870ba5f6458
FrancescoSTL <francescosapolizzi@gmail.com>
2015-09-19 01:50:52 -0400

    Branded and added interaction/citation page

---
+++ FedUp/Controllers/InteractionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FedUp.Controllers
+{
+    public class InteractionController : Controller
+    {
+        // GET: Interaction
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Citation()
+        {
+
+            return View();
+        }
+    }
+}
