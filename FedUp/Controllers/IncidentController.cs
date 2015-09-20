ca9d02431929bca30ff5ec567e91c454ff9d8dc2
FrancescoSTL <francescosapolizzi@gmail.com>
2015-09-20 02:58:24 -0400

    Connected w/Firebase and read/write w/ Dropbox API.

--- FedUp/Controllers/IncidentController.cs
+++ FedUp/Controllers/IncidentController.cs
@@ -1,48 +1,152 @@
-﻿using System;
+﻿using DropboxRestAPI;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Fedup.Controllers
 {
+
     public class IncidentController : Controller
     {
         public struct incident {
-            public double longitute;
+            public double longitude;
             public double latitude;
-            public DateTime timestamp;
-            public string dropBoxURL;
+            public int timestamp;
+            public string file_name;
         }
 
         // GET: Incident
         [AllowAnonymous]
-        public ActionResult Index()
+        [HttpGet]
+        public async Task<ActionResult> Index()
         {
+                //initialize dropbox auth options
+            var options = new Options
+            {
+                ClientId = "rnizd2wt4vhv4cn",
+                ClientSecret = "k8exfknbce45n5g",
+                RedirectUri = "https://fedup.azurewebsites.net/Incident"
+                //RedirectUri = "http://localhost:49668/Incident"
+            };
+
+                // Initialize a new Client (without an AccessToken)
+            var dropboxClient = new Client(options);
+
+                // Get the OAuth Request Url
+            var authRequestUrl = await dropboxClient.Core.OAuth2.AuthorizeAsync("code");
+
+            if (Request.QueryString["code"] == null)
+            {
+                    // Navigate to authRequestUrl using the browser, and retrieve the Authorization Code from the response
+                Response.Redirect(authRequestUrl.ToString());
+            }
+                //get authcode from querstring param
+            var authCode = Request.QueryString["code"];
+
+            //    // Exchange the Authorization Code with Access/Refresh tokens
+            //var token = await dropboxClient.Core.OAuth2.TokenAsync(authCode);
+
+            //// Get root folder without content
+            ////var appFolder = await dropboxClient.Core.Metadata.MetadataAsync("/Apps/FedUpApp", list: true);
+            ////Console.WriteLine("Root Folder: {0} (Id: {1})", appFolder.Name, appFolder.path);
+
+            //// Initialize a new Client (with an AccessToken)
+            //var client2 = new Client(options);
+
+            ////    // Get root folder with content
+            //var rootFolder = await client2.Core.Metadata.MetadataAsync("/", list: false);
+            //foreach (var folder in rootFolder.contents)
+            //{
+            //    Console.WriteLine(" -> {0}: {1} (Id: {2})", folder.is_dir ? "Folder" : "File", folder.Name, folder.path);
+            //}
+
+            //    // Find a file in the root folder
+            //var file = rootFolder.contents.FirstOrDefault(x => x.is_dir == false);
+
+            //// Download a file
+            //var tempFile = Path.GetTempFileName();
+            //using (var fileStream = System.IO.File.OpenWrite(tempFile))
+            //{
+            //    await client2.Core.Metadata.FilesAsync(file.path, fileStream);
+            //}
+
+            // Exchange the Authorization Code with Access/Refresh tokens
+            var token = await dropboxClient.Core.OAuth2.TokenAsync(authCode);
+
+            // Get account info
+            var accountInfo = await dropboxClient.Core.Accounts.AccountInfoAsync();
+            Console.WriteLine("Uid: " + accountInfo.uid);
+            Console.WriteLine("Display_name: " + accountInfo.display_name);
+            Console.WriteLine("Email: " + accountInfo.email);
+
+            // Get root folder without content
+            var rootFolder = await dropboxClient.Core.Metadata.MetadataAsync("/", list: false);
+            Console.WriteLine("Root Folder: {0} (Id: {1})", rootFolder.Name, rootFolder.path);
+
+            // Get root folder with content
+            rootFolder = await dropboxClient.Core.Metadata.MetadataAsync("/", list: true);
+            foreach (var folder in rootFolder.contents)
+            {
+                Console.WriteLine(" -> {0}: {1} (Id: {2})",
+                    folder.is_dir ? "Folder" : "File", folder.Name, folder.path);
+            }
+
+            // Initialize a new Client (with an AccessToken)
+            var client2 = new Client(options);
+
+            // Find a file in the root folder
+            var file = rootFolder.contents.FirstOrDefault(x => x.is_dir == false);
+
+            // Download a file
+            var tempFile = Path.GetTempFileName();
+            using (var fileStream = System.IO.File.OpenWrite(tempFile))
+            {
+                await client2.Core.Metadata.FilesAsync(file.path, fileStream);
+            }
+
+            @ViewBag.filepath = file.path;
+
             List<incident> Incidents = new List<incident>();
 
-            incident incident1 = new incident();
-            incident incident2 = new incident();
-            incident incident3 = new incident();
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "0dwRwB8V2QmJTM9NbKtWfqlys91LwZguvE67oS1f",
+                BasePath = "https://fedup.firebaseio.com/"
+            };
 
-            incident1.latitude = 51.5033630;
-            incident1.longitute = -0.1276250;
-            incident1.timestamp = new DateTime(2015, 9, 15, 7, 0, 0);
-            incident1.dropBoxURL = "about:home";
+                //initialize new firebase client
+            IFirebaseClient client = new FirebaseClient(config);
 
-            incident2.latitude = 38.6043618;
-            incident2.longitute = -90.5857429;
-            incident2.timestamp = new DateTime(2015, 9, 1, 7, 0, 0);
-            incident2.dropBoxURL = "about:home";
+            bool hasNext = true;
+            int counter = 1;
+            do
+            {
+                try
+                {
+                    //pull current row
+                    FirebaseResponse response = await client.GetAsync(counter.ToString());
 
-            incident3.latitude = 37.7892969;
-            incident3.longitute = -122.3892897;
-            incident3.timestamp = new DateTime(2015, 9, 5, 7, 0, 0);
-            incident3.dropBoxURL = "about:home";
-
-            Incidents.Add(incident1);
-            Incidents.Add(incident2);
-            Incidents.Add(incident3);
+                    //parse our json object
+                    incident jsonObject = JsonConvert.DeserializeObject<incident>(response.Body);
+                    Incidents.Add(jsonObject);
+                }
+                catch
+                {
+                    hasNext = false;
+                    break;
+                }
+                counter++;
+            }
+            while (hasNext);
 
             ViewBag.Incidents = Incidents;
 
